3dabccf5317de970f78bd53dc159d723afe1ac62
logaegae <logaegae@gmail.com>
2017-09-10 17:35:06 +0900

    20170909

---
+++ service.php
@@ -0,0 +1,134 @@
+<!-- HEADER -->
+<?php include("./include/header.php"); ?>
+<!-- /.header -->
+
+
+<!-- WRAPPER -->
+<div class="wrapper">
+
+	<!-- PAGE TITLE -->
+	<header class="pagetitle pagetitle-sm">
+		<div class="container">
+			<div class="divtable irow">
+				<!-- <div class="divcell">
+					<h1>체험하기</h1>
+				</div> -->
+				<div class="divcell text-right">
+					<ol class="breadcrumb">
+						<li><a href="index.php">Home</a></li>
+						<li class="active"><span>데모</span></li>
+					</ol>
+				</div>
+			</div>
+		</div>
+	</header>
+	<!-- /.pagetitle -->
+
+	<!-- CONTENT -->
+	<div class="content mix-wrap">
+
+		<!-- CONTAINER -->
+		<div class="container">
+
+			<div class="row irow-xs">
+				<div class="col-sm-4">
+					<h2>레퍼런스</h2>
+				</div>
+				<div class="col-sm-8 text-right">
+					<ul class="mix-category">
+
+						<li class="active"><a href="#all" class="filter active" data-filter="*">전체</a></li>
+						<li><a href="#fn" class="filter" data-filter=".fn">기능</a></li>
+						<!--
+						<li><a href="#portfolio" class="filter" data-filter=".portfolio">포트폴리오</a></li>
+						<li><a href="#menu" class="filter" data-filter=".menu">메뉴판</a></li>
+						<li><a href="#marketing" class="filter" data-filter=".marketing">홍보·마케팅</a></li>
+						<li><a href="#personal" class="filter" data-filter=".personal">개인페이지</a></li> -->
+					</ul>
+				</div>
+			</div>
+		</div>
+		<!-- /.container -->
+
+		<!-- CONTAINER -->
+		<div class="container">
+			<div class="row isotope-wrap gallery-wrap magnific-wrap margin-wrap">
+				<div class="col-xs-6 mix fn">
+					<figure class="figure">
+						<img src="workspace/reference/img/basics03.jpg" alt="">
+						<a href="/workspace/reference/ref01.html" class="mask mask-info mask-dark text-center">
+							<div class="mask-info">
+								<h4>기능1</h4>
+								<div class="heading-divider"></div>
+							</div>
+						</a>
+					</figure>
+				</div>
+				<div class="col-xs-6 mix fn">
+					<figure class="figure">
+						<img src="http://placehold.it/2000x1333" alt="">
+						<a href="" class="mask mask-info mask-dark text-center">
+							<div class="mask-info">
+								<h4>기능2</h4>
+								<div class="heading-divider"></div>
+							</div>
+						</a>
+					</figure>
+				</div>
+				<div class="col-xs-6 mix fn">
+					<figure class="figure">
+						<img src="http://placehold.it/2000x1333" alt="">
+						<a href="" class="mask mask-info mask-dark text-center">
+							<div class="mask-info">
+								<h4>기능3</h4>
+								<div class="heading-divider"></div>
+							</div>
+						</a>
+					</figure>
+				</div>
+				<div class="col-xs-6 mix fn">
+					<figure class="figure">
+						<img src="http://placehold.it/2000x1333" alt="">
+						<a href="" class="mask mask-info mask-dark text-center">
+							<div class="mask-info">
+								<h4>기능4</h4>
+								<div class="heading-divider"></div>
+							</div>
+						</a>
+					</figure>
+				</div>
+				<div class="col-xs-6 mix fn">
+					<figure class="figure">
+						<img src="http://placehold.it/2000x1333" alt="">
+						<a href="" class="mask mask-info mask-dark text-center">
+							<div class="mask-info">
+								<h4>기능5</h4>
+								<div class="heading-divider"></div>
+							</div>
+						</a>
+					</figure>
+				</div>
+				<div class="col-xs-6 mix personal">
+					<figure class="figure">
+						<img src="http://placehold.it/2000x1333" alt="">
+						<a href="" class="mask mask-info mask-dark text-center">
+							<div class="mask-info">
+								<h4>기능6</h4>
+								<div class="heading-divider"></div>
+							</div>
+						</a>
+					</figure>
+				</div>
+			</div>
+		</div>
+		<!-- /.container -->
+
+	</div>
+	<!-- /.content -->
+
+</div>
+<!-- /.wrapper -->
+
+<!-- footer -->
+<?php include("./include/footer.php"); ?>
+<!-- /.footer -->
