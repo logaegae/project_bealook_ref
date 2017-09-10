f115d80e636a084f2dea89a5433d965b11e28f92
cherie1214 <cherie@choeliyeong-ui-MacBook-Pro.local>
2017-09-10 13:50:14 +0900

    test

--- html/index.php
+++ html/index.php
@@ -16,7 +16,7 @@
 				</li>
 				<li data-transition="fade" data-slotamount="7" data-saveperformance="on" class="background overlay overlay-light" style="background-image: url(./static/images/index/s2.jpg);">
 					<div class="tp-caption sft large_bold_black"  data-x="left" data-y="center" data-hoffset="100" data-voffset="-85" data-speed="700" data-start="700" data-easing="easeOutBack">필요한 건 뭐든지<span class="tit-eng">Whatever You Need</span></div>
-					<p class="tp-caption fade" data-x="left" data-y="center" data-hoffset="100" data-speed="500" data-start="900" data-easing="easeOutBack">SNS마켓 / 쇼핑몰 / 홍보 / 메뉴판 / 포트폴리오 / 청첩장 / 개인 웹페이지 등 다양하게</p>
+					<p class="tp-caption fade" data-x="left" data-y="center" data-hoffset="100" data-speed="500" data-start="900" data-easing="easeOutBack">?SNS마켓 / 쇼핑몰 / 홍보 / 메뉴판 / 포트폴리오 / 청첩장 / 개인 웹페이지 등 다양하게</p>
 					<a href="service.php" class="tp-caption sfb btn btn-primary btn-lg btn-wide slider-scroll" data-x="left" data-y="center" data-voffset="80" data-hoffset="100" data-speed="300" data-start="1000">How To Use</a>
 				</li>
 				<li data-transition="fade" data-slotamount="7" data-saveperformance="on" class="background overlay overlay-light" style="background-image: url(./static/images/index/s1.jpg);">
