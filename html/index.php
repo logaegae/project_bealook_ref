8802f0d588d894d1afb8539e1a18553935b2827b
logaegae <logaegae@gmail.com>
2017-09-27 22:12:01 +0900

    20170927

--- html/index.php
+++ html/index.php
@@ -19,11 +19,13 @@
 
 	<div class="main-slider">
 		<div class="logo-box"><img src="./static/images/index/main_banner_bealook.png"></div>
-		<div class="img-wrapper owl-carousel">
-			<div style="background-image: url(./static/images/index/main_bg_02_2.jpg);"></div>
-			<div style="background-image: url(./static/images/index/main_bg_01.jpg);"></div>
-			<div style="background-image: url(./static/images/index/main_bg_07.jpg);"></div>
-			<div style="background-image: url(./static/images/index/main_bg_08.jpg);"></div>
+		<div class="img-wrapper">
+			<ul class="owl-carousel">
+				<li style="background-image: url(./static/images/index/main_bg_02_2.jpg);"></li>
+				<li style="background-image: url(./static/images/index/main_bg_01.jpg);"></li>
+				<li style="background-image: url(./static/images/index/main_bg_07.jpg);"></li>
+				<li style="background-image: url(./static/images/index/main_bg_08.jpg);"></li>
+			</ul>
 		</div>
 	</div>
 
