de9ab9a93065016cb238fdb66169a50311509412
logaegae <logaegae@gmail.com>
2017-10-02 21:29:43 +0900

    20171002

---
+++ html/demo/test/js/tool.js
@@ -0,0 +1,73 @@
+var depth1 = `
+<div class="tool-common tool-txt tool-txt-click bl hTDepth1">
+    <div class="select-box">
+        <label for="fontFamily">Noto Sans</label>
+        <select name="s01" id="fontFamily" required="required">
+            <option selected>Noto Sans</option>
+            <option>나눔고딕</option>
+            <option>나눔바른고딕</option>
+            <option>한겨례체</option>
+            <option>정음체</option>
+            <option>대한체</option>
+            <option>이롭게바탕체</option>
+        </select>
+    </div>
+    <div class="select-box">
+        <label for="fontSize">제목 1 (30pt)</label>
+        <select name="s02" id="fontSize" required="required">
+            <option>제목 1 (30pt) </option>
+            <option>제목 2 (25pt)</option>
+            <option>제목 3 (20pt)</option>
+            <option>본문 1 (15pt)</option>
+            <option>본문 2 (12pt)</option>
+        </select>
+    </div>
+    <button type="button" title="두껍게" exec="bold" class="d1noArgBtn btn btn-icon btn-bold">두껍게</button>
+    <button type="button" title="이탤릭" exec="italic" class="d1noArgBtn btn btn-icon btn-italic">이탤릭</button>
+    <button type="button" title="가운데 줄" exec="StrikeThrough" class="d1noArgBtn btn btn-icon btn-linethrough">가운데 줄</button>
+    <button type="button" title="밑 줄" exec="underline" class="d1noArgBtn btn btn-icon btn-underline">밑 줄</button>
+    <button type="button" title="글씨 색" exec="foreColor" class="d1noArgBtn btn btn-icon btn-color bl">글씨 색</button>
+    <button type="button" title="좌측 정렬" exec="justifyleft" class="d1noArgBtn btn btn-icon btn-ta-left bl">좌측 정렬</button>
+    <button type="button" title="가운데 정렬" exec="justifycenter" class="d1noArgBtn btn btn-icon btn-ta-center">가운데 정렬</button>
+    <button type="button" title="우측 정렬" exec="justifyright" class="d1noArgBtn btn btn-icon btn-ta-right br">우측 정렬</button>
+</div>
+`;
+var depth2 = `
+<div class="tool-common tool-txt tool-txt-drag bl hTDepth2">
+    <div class="select-box">
+        <label for="fontFamily">Noto Sans</label>
+        <select name="s01" id="fontFamily" required="required">
+            <option selected>Noto Sans</option>
+            <option>나눔고딕</option>
+            <option>나눔바른고딕</option>
+            <option>한겨례체</option>
+            <option>정음체</option>
+            <option>대한체</option>
+            <option>이롭게바탕체</option>
+        </select>
+    </div>
+    <div class="select-box">
+        <label for="fontSize">제목 1 (30pt)</label>
+        <select name="s02" id="fontSize" required="required">
+            <option>제목 1 (30pt) </option>
+            <option>제목 2 (25pt)</option>
+            <option>제목 3 (20pt)</option>
+            <option>본문 1 (15pt)</option>
+            <option>본문 2 (12pt)</option>
+        </select>
+    </div>
+    <button type="button" title="두껍게" exec="bold" class="btn btn-icon btn-bold d2noArgBtn">두껍게</button>
+    <button type="button" title="이탤릭" exec="italic" class="btn btn-icon btn-italic d2noArgBtn">이탤릭</button>
+    <button type="button" title="가운데 줄" exec="StrikeThrough" class="btn btn-icon btn-linethrough d2noArgBtn">가운데 줄</button>
+    <button type="button" title="밑 줄" exec="underline" class="btn btn-icon btn-underline d2noArgBtn">밑 줄</button>
+    <button type="button" title="글씨 색" exec="foreColor" class="btn btn-icon btn-color bl d2noArgBtn">글씨 색</button>
+    <button type="button" title="배경 " exec="backColor" class="btn btn-icon btn-bgcolor d2noArgBtn">배경 색</button>
+    <button type="button"  title="글머리 불릿" exec="insertUnorderedList" class="btn btn-icon btn-ul bl d2noArgBtn">글머리 불릿</button>
+    <button type="button" title="글머리 숫자" exec="insertOrderedList" class="btn btn-icon btn-ol">글머리 숫자</button>
+    <button type="button" title="링크 걸기" exec="createLink" class="btn btn-icon btn-link2 bl br">링크 걸기</button>
+</div>
+`;
+$(function() {
+    $("body").prepend(depth1);
+    $("body").prepend(depth2);
+})
