d1c744150764a139e6104a2183bc82dcd30f801e
logaegae <logaegae@gmail.com>
2017-10-02 17:23:04 +0900

    20171002

--- html/admin/index.php
+++ html/admin/index.php
@@ -28,31 +28,8 @@ if(isset($_SESSION["id"])){
 		$mysqli->close();
 	}
 }
-?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
-	<title>BeALook</title>
-	<meta name="author" content="bealook" />
-	<meta name="description" content="비어룩 마켓 서비스, 인스타그램, 페이스북, 블로그, 카카오, 기타 SNS 마켓 쇼핑몰 플랫폼. 스토리텔링, 장인, 소상공인을 지원합니다." />
-	<meta name="keywords"  content="비어룩, bealook, instagram, facebook. kakao, market, 쇼핑몰, 마켓, 인스타마켓, 블로그마켓, 장인, 소상공인" />
-	<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic&amp;subset=latin,cyrillic" rel="stylesheet">
-	<link href="http://fonts.googleapis.com/css?family=Montserrat:700" rel="stylesheet">
-	<link href="http://fonts.googleapis.com/css?family=Satisfy" rel="stylesheet">
-	<link href="../static/css/preloader.css" rel="stylesheet">
-	<link href="../static/css/bootstrap.css" rel="stylesheet">
-	<link href="../static/css/font-awesome.min.css" rel="stylesheet">
-	<link href="../static/css/animate.css" rel="stylesheet">
-	<link href="../static/css/revolution.css" rel="stylesheet">
-	<link href="../static/css/style.css" rel="stylesheet">
-</head>
-<body>
-
-<!-- Preloader -->
-<div class="preloader"></div>
+include("../include/adminHeader.php");?>
 
 
 <!-- WRAPPER -->
